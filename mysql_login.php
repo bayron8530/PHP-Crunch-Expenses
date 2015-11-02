6ac4bacb2f3a98261345e748b5f751adcc3a25e4
bayron8530 <bayron.sanchezmu@amigo.edu.co>
2015-11-02 13:22:10 -0500

    Create mysql_login.php
    Este archivo va dentro de una carpeta llamada data en el proyecto php

---
+++ mysql_login.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("PORT", ":8080");// Número del puerto [ Opcional ]
+define("DATABASE", "crunch_expenses"); // Nombre de la base de datos
+define("USERNAME", "bayron"); // Nombre del usuario
+define("PASSWORD", "Byron72."); // Constraseña
+
+?>
